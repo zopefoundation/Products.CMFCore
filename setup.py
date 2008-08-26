7bc8ed6b35ad0024a1c69967b4a2eba8fa49813c
Jens Vagelpohl <jens@dataflake.org>
2008-08-26 11:25:58 +0000

    - pinning GS to 1.3.3 - refactor setup.py so it looks like the trunk's - reformat README for PyPI prettiness - synthesized CHANGES.txt from the monolithic file at   http://svn.zope.org/CMF/branches/2.1/CHANGES.txt?rev=87755&view=auto

--- setup.py
+++ setup.py
@@ -2,29 +2,25 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
+NAME = 'CMFCore'
+
 here = os.path.abspath(os.path.dirname(__file__))
-package = os.path.join(here, 'Products', 'CMFCore')
+package = os.path.join(here, 'Products', NAME)
 
 def _package_doc(name):
     f = open(os.path.join(package, name))
     return f.read()
 
-NAME = 'CMFCore'
-
-VERSION = _package_doc('version.txt').strip()
-if VERSION.startswith(NAME):
-    VERSION = VERSION[len(NAME):]
-while VERSION and VERSION[0] in '-_.':
-    VERSION = VERSION[1:]
-
-_boundary = '\n' + ('-' * 60) + '\n'
-README = (open(os.path.join(here, 'README.txt')).read()
-        + _boundary + _package_doc('README.txt')
-        + _boundary + _package_doc('CHANGES.txt')
+_boundary = '\n' + ('-' * 60) + '\n\n'
+README = (_package_doc('README.txt')
+         + _boundary
+         + _package_doc('CHANGES.txt')
+         + _boundary
+         + "Download\n========"
          )
 
-setup(name='Products.CMFCore',
-      version=VERSION,
+setup(name='Products.%s' % NAME,
+      version=_package_doc('version.txt').strip(),
       description='Core components of Zope Content Management Framework',
       long_description=README,
       classifiers=[
@@ -39,16 +35,21 @@ setup(name='Products.CMFCore',
         ],
       keywords='web application server zope zope2 cmf',
       author="Zope Corporation and contributors",
-      author_email="zope-cmf@lists.zope.org",
-      url="http://www.zope.org/Products/CMFCore",
+      author_email="zope-cmf@zope.org",
+      url="http://pypi.python.org/pypi/Products.CMFCore",
       license="ZPL 2.1 (http://www.zope.org/Resources/License/ZPL-2.1)",
       packages=find_packages(),
       include_package_data=True,
       namespace_packages=['Products'],
       zip_safe=False,
-      #install_requires=['Zope >= 2.10']
+      install_requires=[
+        #'Zope >= 2.10.4',
+        'setuptools',
+        'five.localsitemanager>=0.3',
+        'Products.GenericSetup==1.3.3',
+        ],
       entry_points="""
       [zope2.initialize]
-      Products.CMFCore = Products.CMFCore:initialize
-      """,
+      Products.%s = Products.%s:initialize
+      """ % (NAME, NAME),
       )
