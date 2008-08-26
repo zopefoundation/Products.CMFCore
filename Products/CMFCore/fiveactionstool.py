791a600a568e5e1aaac353515f7665eb2aea63d1
Jens Vagelpohl <jens@dataflake.org>
2008-08-26 14:34:16 +0000

    - fiveactionstool: fix typo in a method name that isn't even used in the CMF   or Plone.

--- Products/CMFCore/fiveactionstool.py
+++ Products/CMFCore/fiveactionstool.py
@@ -41,7 +41,7 @@ class FiveActionsTool( UniqueObject, SimpleItem, ActionProviderBase ):
 
     security = ClassSecurityInfo()
 
-    def getReqestURL(self):
+    def getRequestURL(self):
         return self.REQUEST.URL
 
     security.declarePrivate('listActions')
