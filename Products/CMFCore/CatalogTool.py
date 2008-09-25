427e48a6e946ed2191947c24636108ca892b02cf
Jens Vagelpohl <jens@dataflake.org>
2008-09-25 20:30:28 +0000

    - CatalogTool: If proxy roles are set on a script that uses the catalog   and those proxy roles have been unset using the ZMI, which results   in an empty tuple as proxy roles, then the catalog would not correctly   determine what the current user is allowed to see.   (https://bugs.launchpad.net/zope-cmf/+bug/161729)

--- Products/CMFCore/CatalogTool.py
+++ Products/CMFCore/CatalogTool.py
@@ -143,7 +143,7 @@ class CatalogTool(UniqueObject, ZCatalog, ActionProviderBase):
         if sm.calledByExecutable():
             eo = sm._context.stack[-1]
             proxy_roles = getattr(eo, '_proxy_roles', None)
-            if proxy_roles is not None:
+            if proxy_roles:
                 effective_roles = proxy_roles
         result = list( effective_roles )
         result.append( 'Anonymous' )
