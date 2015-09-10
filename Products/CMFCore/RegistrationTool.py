e1d981bfa14b664317285f0f36498f4be4a23406
vangheem <vangheem@gmail.com>
2015-09-10 11:16:42 -0500

    Make sure RegistrationTool.addMember is not published

--- Products/CMFCore/RegistrationTool.py
+++ Products/CMFCore/RegistrationTool.py
@@ -132,14 +132,17 @@ class RegistrationTool(UniqueObject, SimpleItem):
     @postonly
     def addMember(self, id, password, roles=('Member',), domains='',
                   properties=None, REQUEST=None):
-        '''Creates a PortalMember and returns it. The properties argument
-        can be a mapping with additional member properties. Raises an
-        exception if the given id already exists, the password does not
-        comply with the policy in effect, or the authenticated user is not
-        allowed to grant one of the roles listed (where Member is a special
-        role that can always be granted); these conditions should be
-        detected before the fact so that a cleaner message can be printed.
-        '''
+        # XXX Do not make this a normal method comment. Doing so makes
+        # this method publishable
+
+        # Creates a PortalMember and returns it. The properties argument
+        # can be a mapping with additional member properties. Raises an
+        # exception if the given id already exists, the password does not
+        # comply with the policy in effect, or the authenticated user is not
+        # allowed to grant one of the roles listed (where Member is a special
+        # role that can always be granted); these conditions should be
+        # detected before the fact so that a cleaner message can be printed.
+
         if not self.isMemberIdAllowed(id):
             raise ValueError(_(u'The login name you selected is already in '
                                u'use or is not valid. Please choose another.'))
