894f61fb1389342022c39a5cd6ab1b64f6165a29
Gil Forcada <gforcada@gnome.org>
2016-05-17 11:15:02 +0200

    Allow to disable catalog optimization via an environment variable
    This is mostly meant to be used by testing/CI where suddenly some tests
    can start failing as soon as this code got merged.
    
    Part of PLIP: https://github.com/plone/Products.CMFPlone/issues/1343

--- Products/CMFCore/CatalogTool.py
+++ Products/CMFCore/CatalogTool.py
@@ -47,10 +47,21 @@ from Products.CMFCore.utils import _mergedLocalRoles
 from Products.CMFCore.utils import getToolByName
 from Products.CMFCore.utils import UniqueObject
 
+import os
+
+
+CATALOG_OPTIMIZATION_DISABLED = os.environ.get(
+    'CATALOG_OPTIMIZATION_DISABLED',
+    'false'
+)
+CATALOG_OPTIMIZATION_DISABLED = CATALOG_OPTIMIZATION_DISABLED.lower() in \
+    ('true', 't', 'yes', 'y', '1')
+
+
 class IndexableObjectSpecification(ObjectSpecificationDescriptor):
 
     # This class makes the wrapper transparent, adapter lookup is
-    # carried out based on the interfaces of the wrapped object. 
+    # carried out based on the interfaces of the wrapped object.
 
     def __get__(self, inst, cls=None):
         if inst is None:
@@ -272,17 +283,23 @@ class CatalogTool(UniqueObject, ZCatalog, ActionProviderBase):
 
     security.declarePrivate('indexObject')
     def indexObject(self, object):
-        obj = filterTemporaryItems(object)
-        indexer = getQueue()
-        if obj is not None and indexer is not None:
-            indexer.index(obj)
+        if not CATALOG_OPTIMIZATION_DISABLED:
+            obj = filterTemporaryItems(object)
+            indexer = getQueue()
+            if obj is not None and indexer is not None:
+                indexer.index(obj)
+        else:
+            self._indexObject(object)
 
     security.declarePrivate('unindexObject')
     def unindexObject(self, object):
-        obj = filterTemporaryItems(object, checkId=False)
-        indexer = getQueue()
-        if obj is not None and indexer is not None:
-            indexer.unindex(obj)
+        if not CATALOG_OPTIMIZATION_DISABLED:
+            obj = filterTemporaryItems(object, checkId=False)
+            indexer = getQueue()
+            if obj is not None and indexer is not None:
+                indexer.unindex(obj)
+        else:
+            self._unindexObject(object)
 
     security.declarePrivate('reindexObject')
     def reindexObject(self, object, idxs=[], update_metadata=1, uid=None):
@@ -290,12 +307,21 @@ class CatalogTool(UniqueObject, ZCatalog, ActionProviderBase):
         # of the object for the "reindex all" case.  unfortunately, some other
         # packages like `CMFEditions` check that date to see if the object was
         # modified during the request, which fails when it's only set on commit
-        if idxs in (None, []) and hasattr(aq_base(object), 'notifyModified'):
-            self.notifyModified()
-        obj = filterTemporaryItems(object)
-        indexer = getQueue()
-        if obj is not None and indexer is not None:
-            indexer.reindex(obj, idxs, update_metadata=update_metadata)
+        if not CATALOG_OPTIMIZATION_DISABLED:
+            if idxs in (None, []) and \
+                    hasattr(aq_base(object), 'notifyModified'):
+                self.notifyModified()
+            obj = filterTemporaryItems(object)
+            indexer = getQueue()
+            if obj is not None and indexer is not None:
+                indexer.reindex(obj, idxs, update_metadata=update_metadata)
+        else:
+            self._reindexObject(
+                object,
+                idxs=idxs,
+                update_metadata=update_metadata,
+                uid=uid
+            )
 
     security.declarePrivate('_indexObject')
     def _indexObject(self, object):
