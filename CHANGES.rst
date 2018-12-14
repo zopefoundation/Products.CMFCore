7a478973d40abe951d8096b1887790cf54ac090c
Michael Howitz <mh@gocept.com>
2018-12-14 15:05:11 +0100

    Preparing release 2.4.0b6

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Products.CMFCore Changelog
 ==========================
 
-2.4.0b6 (unreleased)
+2.4.0b6 (2018-12-14)
 --------------------
 
 - Change `im_self` to `__self__` for Python 3 compatibility.
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ README = _boundary.join([
 ])
 
 setup(name='Products.%s' % NAME,
-      version='2.4.0b6.dev0',
+      version='2.4.0b6',
       description='Zope Content Management Framework core components',
       long_description=README,
       classifiers=[
