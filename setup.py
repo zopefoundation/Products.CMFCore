88117c21dc3aab62886e1a1abef7febe9b02eb8f
Jens Vagelpohl <jens@netz.ooo>
2020-01-29 19:09:33 -0600

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ README = _boundary.join([
 ])
 
 setup(name='Products.%s' % NAME,
-      version='2.4.4',
+      version='2.4.5.dev0',
       description='Zope Content Management Framework core components',
       long_description=README,
       classifiers=[
