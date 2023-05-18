4de49b35ab9e269cccc557491d732b5ec10208e2
Roel Bruggink <roel@jaroel.nl>
2023-05-18 19:36:34 +0200

    lint

--- src/Products/CMFCore/explicitacquisition.py
+++ src/Products/CMFCore/explicitacquisition.py
@@ -9,16 +9,12 @@ from Products.CMFCore.interfaces import IPublishableThroughAcquisition
 from Products.CMFCore.interfaces import IShouldAllowAcquiredItemPublication
 
 
-PUBLISHABLE_THROUGH_ACQUISITION = (
-    os.environ.get("ALLOW_CONTENT_PUBLISHABLE_THROUGH_ACQUISITION", "false") == "true"
-)
+PTA = os.environ.get("PUBLISHING_EXPLICIT_ACQUISITION", "false") == "true"
 
 
 @adapter(IPubAfterTraversal)
 def after_traversal_hook(event):
-    if PUBLISHABLE_THROUGH_ACQUISITION or IPublishableThroughAcquisition.providedBy(
-        event.request
-    ):
+    if PTA or IPublishableThroughAcquisition.providedBy(event.request):
         return
     context = event.request["PARENTS"][0]
     if IShouldAllowAcquiredItemPublication(context, None) is False:
