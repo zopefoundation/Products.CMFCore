fff7f63ea8660ed917de42a416c8425b1bb291a4
Roel Bruggink <roel@jaroel.nl>
2023-05-18 19:28:43 +0200

    lint

--- src/Products/CMFCore/explicitacquisition.py
+++ src/Products/CMFCore/explicitacquisition.py
@@ -1,3 +1,5 @@
+import os
+
 from zExceptions import NotFound
 from zope.component import adapter
 from ZPublisher.interfaces import IPubAfterTraversal
@@ -5,13 +7,18 @@ from ZPublisher.interfaces import IPubAfterTraversal
 from Products.CMFCore.interfaces import IContentish
 from Products.CMFCore.interfaces import IPublishableThroughAcquisition
 from Products.CMFCore.interfaces import IShouldAllowAcquiredItemPublication
-import os
 
-PUBLISHABLE_THROUGH_ACQUISITION =  os.environ.get("ALLOW_CONTENT_PUBLISHABLE_THROUGH_ACQUISITION", "false") == "true"
+
+PUBLISHABLE_THROUGH_ACQUISITION = (
+    os.environ.get("ALLOW_CONTENT_PUBLISHABLE_THROUGH_ACQUISITION", "false") == "true"
+)
+
 
 @adapter(IPubAfterTraversal)
 def after_traversal_hook(event):
-    if  PUBLISHABLE_THROUGH_ACQUISITION or IPublishableThroughAcquisition.providedBy(event.request):
+    if PUBLISHABLE_THROUGH_ACQUISITION or IPublishableThroughAcquisition.providedBy(
+        event.request
+    ):
         return
     context = event.request["PARENTS"][0]
     if IShouldAllowAcquiredItemPublication(context, None) is False:
