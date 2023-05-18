dd5c268748e882d687980710970b22f0311c1263
Roel Bruggink <roel@jaroel.nl>
2023-05-18 18:36:57 +0200

    Update src/Products/CMFCore/explicitacquisition.py
    Co-authored-by: Jens W. Klein <jk@kleinundpartner.at>

--- src/Products/CMFCore/explicitacquisition.py
+++ src/Products/CMFCore/explicitacquisition.py
@@ -5,11 +5,13 @@ from ZPublisher.interfaces import IPubAfterTraversal
 from Products.CMFCore.interfaces import IContentish
 from Products.CMFCore.interfaces import IPublishableThroughAcquisition
 from Products.CMFCore.interfaces import IShouldAllowAcquiredItemPublication
+import os
 
+PUBLISHABLE_THROUGH_ACQUISITION =  os.environ.get("ALLOW_CONTENT_PUBLISHABLE_THROUGH_ACQUISITION", "false") == "true"
 
 @adapter(IPubAfterTraversal)
 def after_traversal_hook(event):
-    if IPublishableThroughAcquisition.providedBy(event.request):
+    if  PUBLISHABLE_THROUGH_ACQUISITION or IPublishableThroughAcquisition.providedBy(event.request):
         return
     context = event.request["PARENTS"][0]
     if IShouldAllowAcquiredItemPublication(context, None) is False:
