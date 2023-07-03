113155e0333a520abe029a29a0e9862097882ea4
Jens Vagelpohl <jens@plyp.com>
2023-07-03 11:58:00 +0200

    - vb [ci skip]# Please enter the commit message for your changes. Lines starting

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ README = _boundary.join([
 ])
 
 setup(name='Products.%s' % NAME,
-      version='3.2',
+      version='3.3.dev0',
       description='Zope Content Management Framework core components',
       long_description=README,
       classifiers=[
